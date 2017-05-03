875682ca018729e0bd417a20c070768b32c26cde
Itzel <itzeljimenez.do@gmail.com>
2017-05-03 16:26:10 +0000

    prueba de click en imagenes

--- js/app.js
+++ js/app.js
@@ -1,3 +1,10 @@
 /*
  * Archivo principal de funcionalidad de JS
  */
+var imagenes = document.getElementsByClassName("img");
+for(var i=0; i<imagenes.length; i++){
+	imagenes[i].addEventListener("click", prueba)
+}
+function prueba(){
+	alert("Te necesito!!!!!!")
+}
